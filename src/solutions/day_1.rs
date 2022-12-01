6a75cb7495f6ae3dc9fd88df5c4a5a39e6ec36a7
Archiyopp <crafernandezbe@unal.edu.co>
2022-12-01 04:16:25 -0500

    cleaner first day solution

--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -1,37 +1,25 @@
 pub fn day_1(input: String) -> [i32; 3] {
-    input
-        .lines()
-        .map(|x| x.parse::<i32>().unwrap_or_default())
-        // sum the values separated by 0 anc collect them in a vector
-        .fold(vec![0], |mut acc, x| {
-            if x == 0 {
-                acc.push(0);
-            } else {
-                let sum = acc.pop().expect("vector to have a length > 0") + x;
-                acc.push(sum);
-            }
-            acc
+    let mut vector = input
+        .trim()
+        .split("\r\n\r\n")
+        .map(|s| {
+            s.lines()
+                .map(|x| x.parse::<i32>().unwrap_or_default())
+                .sum()
         })
-        // get the three biggest numbers in the vector
-        .into_iter()
-        .fold([0; 3], |acc, x| {
-            if x > acc[0] {
-                [x, acc[0], acc[1]]
-            } else if x > acc[1] {
-                [acc[0], x, acc[1]]
-            } else if x > acc[2] {
-                [acc[0], acc[1], x]
-            } else {
-                acc
-            }
-        })
+        .collect::<Vec<i32>>();
+    vector.sort();
+    [
+        vector.pop().unwrap_or_default(),
+        vector.pop().unwrap_or_default(),
+        vector.pop().unwrap_or_default(),
+    ]
 }
 
 #[cfg(test)]
 mod tests {
-    use advent_of_code_2022::read_file;
-
     use super::*;
+    use advent_of_code_2022::read_file;
 
     #[test]
     fn test_day_1() {
